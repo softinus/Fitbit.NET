22b2c727995bb022f8515c869d47e304cb324198
softinus <admin@raimsoft.com>
2016-01-23 00:25:14 +0900

    api 매개변수 변경

--- SampleDesktop/Program.cs
+++ SampleDesktop/Program.cs
@@ -13,8 +13,8 @@ namespace SampleDesktop
 {
     class Program
     {
-        const string consumerKey = "YOUR_CONSUMER_KEY_HERE";
-        const string consumerSecret = "YOUR_CONSUMER_SECRET_HERE";
+        const string consumerKey = "1aade6a54fb93f3a7f6542bb00a775be";
+        const string consumerSecret = "56e3b2872f7a97bad0c5686dc2db44e9";
         
         static void Main(string[] args)
         {
@@ -31,8 +31,21 @@ namespace SampleDesktop
 
             var fitbit = new FitbitClient(consumerKey, consumerSecret, credentials.AuthToken, credentials.AuthTokenSecret);
            
-            var profile = fitbit.GetUserProfile();
-            Console.WriteLine("Your last weight was {0}", profile.Weight);
+            var profile = fitbit.GetUserProfile("3LKBDR");
+            Console.WriteLine("표시이름 : {0}", profile.DisplayName);
+            Console.WriteLine("몸무게 : {0}", profile.Weight);
+            Console.WriteLine("이름 : {0}", profile.FullName);
+            Console.WriteLine("성별 : {0}", profile.Gender);
+            Console.WriteLine("키 : {0}{1}", profile.Height, profile.HeightUnit);
+            Console.WriteLine("가입일자 : {0}", profile.MemberSince);
+
+            //fitbit.GetHeartRates(new DateTime(2015, 7, 1));
+            //List<UserProfile> listFriends= fitbit.GetFriends();
+            //foreach(UserProfile UP in listFriends)
+            //{
+            //    Console.WriteLine("친구{0}의 이름 : {1}", 1, UP.DisplayName);
+            //}
+            
 
             Console.ReadLine();
         }
